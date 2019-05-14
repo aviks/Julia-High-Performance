b32b6a0d7bec93edaeb2515de988bdcb25c138c3
Avik Sengupta <avik@sengupta.net>
2019-05-14 10:34:23 +0100

    update chapter 6

--- Chapter6/Chapter6.jl
+++ Chapter6/Chapter6.jl
@@ -49,3 +49,115 @@ b=rand(1000);
 @btime prefix_bounds($a, $b)
 
 @btime prefix_inbounds($a, $b)
+
+## In place operations
+
+function xpow(x)
+   return [x x^2 x^3 x^4]
+end
+
+function xpow_loop(n)
+    s = 0
+    for i = 1:n
+      s = s + xpow(i)[2]
+    end
+   return s
+end
+
+@btime xpow_loop($1000000)
+
+function xpow!(result::Array{Int, 1}, x)
+    @assert length(result) == 4
+    result[1] = x
+    result[2] = x^2
+    result[3] = x^3
+    result[4] = x^4
+end
+
+function xpow_loop_noalloc(n)
+    r = [0, 0, 0, 0]
+    s = 0
+    for i = 1:n
+       xpow!(r, i)
+       s = s + r[2]
+    end
+    s
+end
+@btime xpow_loop_noalloc($1000000)
+
+@time xpow_loop(1_000_000)
+
+@time xpow_loop_noalloc(1_000_000)
+
+@btime sort(a);
+@btime sort!(a);
+
+
+
+#Array Views
+
+function sum_vector(x::Array{Float64, 1})
+   s = 0.0
+   for i = 1:length(x)
+      s = s + x[i]
+   end
+   return s
+end
+
+function sum_cols_matrix(x::Array{Float64, 2})
+   num_cols = size(x, 2)
+   s = zeros(num_cols)
+   for i = 1:num_cols
+      s[i] = sum_vector(x[:, i])
+  end
+  return s
+end
+@benchmark sum_cols_matrix($a)
+
+function sum_vector(x::AbstractArray)
+   s = 0.0
+   for i = 1:length(x)
+      s = s + x[i]
+   end
+   return s
+end
+
+function sum_cols_matrix_views(x::Array{Float64, 2})
+    num_cols = size(x, 2); num_rows = size(x, 1)
+    s = zeros(num_cols)
+    for i = 1:num_cols
+     s[i] = sum_vector(@view(x[:, i]))
+    end
+    return s
+end
+@benchmark sum_cols_matrix_views($a)
+
+
+## SIMD Parallelization
+
+function sum_vectors!(x, y, z)
+    n = length(x)
+    for i = 1:n
+        x[i] = y[i] + z[i]
+    end
+end
+
+
+function sum_vectors_simd!(x, y, z)
+    n = length(x)
+    @inbounds @simd for i = 1:n
+          x[i] = y[i] + z[i]
+    end
+end
+
+a=zeros(Float32, 1_000_000);
+b= rand(Float32, 1_000_000);
+c= rand(Float32, 1_000_000);
+
+
+@btime sum_vectors!($a, $b, $c)
+
+@btime sum_vectors_simd!($a, $b, $c)
+
+@code_llvm sum_vectors_simd!(a, b, c)
+@code_llvm sum_vectors_simd!(a, b, c)
